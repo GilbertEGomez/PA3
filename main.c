c4c85e8522686ea87e8e2ad646e92b0bf42c0333
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-21 23:28:27 +0000

    LRU Added, LRU Algo, LRU Print Statement

--- main.c
+++ main.c
@@ -101,6 +101,53 @@ int FIFO(int* ref_str, int size, int limit){
   return page_faults;
 }
 
+int lru(int* ref_str, int size, int limit){
+  //size is the # of frames allocated for the process
+  //limit is the max # of cells that we look ahead in the RS to implement the optimal algorithm
+  //initialize
+  int page_faults = size;
+  int frames[size], i, cur, page_no, frame_no;
+  for(i = 0; i < size;i++)
+    frames[i] = -1;//empty
+  for(i = 0, cur = 0; i < size;i++,cur++){
+    //filling out the whole physical memory (frames array)
+    page_no = ref_str[cur];
+    frame_no = locate(frames,size, page_no);
+    if(frame_no + 1)//already exists
+      i--;
+    else
+      frames[i] = page_no;
+  }
+  //main loop
+  for(;cur < 1000000;cur++){
+    page_no = ref_str[cur];
+    frame_no = locate(frames, size, page_no);
+    if(frame_no != -1)//already exists
+      continue;
+    //look at ref_str[cur+1:cur+limit] to see which one has not been referred to the longest in the future
+    page_faults++;
+    //This is the very first line that is differnet in other algorithms
+    unsigned unused = (1<<size) - 1;
+    int victim;
+    for(int k = 1; k <= limit && unused && cur - k >= 0;k++){
+      victim = locate(frames, size, ref_str[cur - k]);
+      if(victim == -1)
+        continue;
+      unused &= ~(1<<victim);
+    }
+    if(!unused)
+      frames[victim] = page_no;
+    else{
+      victim = 0;
+      while(unused % 2 == 0)
+        unused = unused >> 1, victim++;
+      frames[victim] = page_no;
+    }    
+  } 
+  
+  return page_faults;
+}
+
 int main(int argc, char** argv) {
   int e, m, P;
   double t;
@@ -146,7 +193,15 @@ int main(int argc, char** argv) {
   }
   int optimal_page_fault = optimal(ref_str, FRAME_NO, e * m);
   int FIFO_page_fault = FIFO(ref_str, FRAME_NO, e * m);
+  int lru_page_fault = lru(ref_str, FRAME_NO, e * m);
   printf("Optimal page replacement causes %d page faults\n", optimal_page_fault);
   printf("FIFO page replacement causes %d page faults\n", FIFO_page_fault);
+  printf("LRU page replacement causes %d page faults\n", lru_page_fault);
   return 0;
 }
+//Scenario 1-5
+//./main -P 1048576 -e 10 -m 20 -t 1000
+//./main -P 4194304 -e 10 -m 20 -t 2000
+//./main -P 1048576 -e 15 -m 20 -t 1000
+//./main -P 4194304 -e 8 -m 50 -t 1000
+//./main -P 262144 -e 15 -m 7 -t 2000
